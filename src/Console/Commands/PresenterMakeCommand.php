33aa8340e4e3b31dc498e1f8270aff6a6dbd0c72
FreedomKnight <fk@unisharp.com>
2015-11-09 12:58:17 +0800

    add for api entity

--- src/Console/Commands/PresenterMakeCommand.php
+++ src/Console/Commands/PresenterMakeCommand.php
@@ -3,6 +3,7 @@
 namespace Unisharp\Unifly\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
 
 class PresenterMakeCommand extends UsGeneratorCommand
 {
@@ -24,11 +25,34 @@ class PresenterMakeCommand extends UsGeneratorCommand
 
     public function getStub()
     {
+        if ($this->for == 'api') {
+            return __DIR__ . '/stubs/Presenter/ApiPresenter.stub';
+        }
+
         return __DIR__ . '/stubs/Presenter/Presenter.stub';
     }
 
     protected function getDefaultNamespace($rootNamespace)
     {
+        if ($this->for == 'api') {
+            $ns = $rootNamespace . '\Presenter';
+            $ns .= '\\' . Str::studly($this->for);
+            return $ns;
+        }
         return $rootNamespace . '\Presenter';
+    }
+
+    protected function getPath($name)
+    {
+        if ($this->for == 'api') {
+            // \App\Presenter\SomePresenter => \App\Presenter\Api\SomePresenter
+            $name = str_replace($this->laravel->getNamespace(), '', $name);
+            $name = explode('\\', $name);
+            array_splice($name, 1, null, 'Api');
+            $name = implode('\\', $name);
+            return $this->laravel['path'].'/'.str_replace('\\', '/', $name).'.php';
+        }
+
+        return parent::getPath($name);
     }
 }
